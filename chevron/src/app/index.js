6e28f8dc5b325a9c4f5f07d401a7377eabf55fb3
Lemmon <lemmon@users.noreply.github.com>
2018-01-21 12:46:13 +0100

    chevron: direction

--- chevron/src/app/index.js
+++ chevron/src/app/index.js
@@ -9,6 +9,25 @@ const code = document.querySelector('.code')
 document.forms[0].addEventListener('input', e => {
   render()
 })
+// select all inputs on focus
+Array.from(document.forms[0].querySelectorAll('input')).forEach(link => {
+  link.addEventListener('focus', (e) => {
+    e.target.select()
+  })
+})
+// select code on focus
+Array.from(document.forms[0].querySelectorAll('[tabindex]')).forEach(link => {
+  link.addEventListener('focus', (e) => {
+    setTimeout(() => {
+      window.getSelection().selectAllChildren(e.target)
+    })
+  })
+  link.addEventListener('mouseup', (e) => {
+    if (window.getSelection().type !== 'Range') {
+      window.getSelection().selectAllChildren(e.target)
+    }
+  })
+})
 
 render()
 
@@ -17,11 +36,35 @@ function render() {
   const height = val('height')
   const maxThickness = Math.floor(Math.sqrt(Math.pow(Math.min(width, height / 2), 2) * 2) / 2)
   const thickness = Math.min(val('thickness'), maxThickness)
+  const direction = document.forms[0].elements['direction'].value
+  const rotate = {
+    left: (width, height) => [ width, height ],
+    right: (width, height) => [ width, height ],
+    up: (width, height) => [ height, width ],
+    down: (width, height) => [ height, width ],
+  }
+  morph(DOM, html`
+    <div class="canvas">
+      <svg width=${width} height=${height} viewBox="0 0 ${width} ${height}">
+        ${path(...rotate[direction](width, height), thickness, direction)}
+      </svg>
+    </body>
+  `)
+  code.innerHTML = escapeHTML(DOM.innerHTML)
+}
+
+function path(width, height, thickness, direction) {
   const a = Math.sqrt(Math.pow(thickness, 2) / 2)
   const b = Math.min(width - a, height / 2)
   const x = (width - a - b) / 2
   const y = (height - b * 2) / 2
-  const d = [
+  const transform = {
+    left: ([ x, y ]) => [ width - x, y ],
+    right: xy => xy,
+    up: ([ x, y ]) => [ y, width - x ],
+    down: ([ x, y ]) => [ y, x ],
+  }
+  const points = [
     [x + a, y],
     [x + b + a, y + b],
     [x + a, height - y],
@@ -30,18 +73,11 @@ function render() {
     [x, y + a],
     [x + a, y],
   ]
-  morph(DOM, html`
-    <div class="canvas">
-      <svg width=${width} height=${height} viewBox="0 0 ${width} ${height}">
-        <path d="${path(d)}" />
-      </svg>
-    </body>
-  `)
-  code.innerHTML = escapeHTML(DOM.innerHTML)
+  return html`<path d="${d(points, transform[direction])}" />`
 }
 
-function path(d) {
-  return 'M' + d[0].join() + d.slice(1).map( a => 'L' + a.join() ).join('') + 'z'
+function d(points, transform) {
+  return 'M' + transform(points[0]).join() + points.slice(1).map( xy => 'L' + transform(xy).join() ).join('') + 'z'
 }
 
 function val(id, def = 320) {
