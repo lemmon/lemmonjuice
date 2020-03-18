4a031a2815e5fa0684b6ea10e108c961a161a346
Lemmon <lemmon@users.noreply.github.com>
2020-03-18 14:03:13 +0100

    portfolio: visual update

--- www/dev/index.php
+++ www/dev/index.php
@@ -7,37 +7,45 @@
   <link rel="shortcut icon" href="https://www.lemmonjuice.com/favicon.ico">
   <link rel="stylesheet" href="https://unpkg.com/abrusco@0.9.0/css/abrusco.min.css">
   <style media="screen">
+  @media (min-width: 1280px) {
+    _body {
+      background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="1280" height="16"> \
+        <g fill="%23b3d4fc" opacity=".2"> \
+          <rect x="48" width="344" height="16"/> \
+          <rect x="424" width="344" height="16"/> \
+        </g> \
+        <g stroke="%23b3d4fc" stroke-dasharray="5 3" opacity=".4"> \
+            <line x1="0.5" y1="0" x2="0.5" y2="16"/> \
+            <line x1="16" y1="0" x2="16" y2="16"/> \
+            <line x1="32" y1="0" x2="32" y2="16"/> \
+            <line x1="48" y1="0" x2="48" y2="16"/> \
+            <line x1="392" y1="0" x2="392" y2="16"/> \
+            <line x1="408" y1="0" x2="408" y2="16"/> \
+            <line x1="424" y1="0" x2="424" y2="16"/> \
+            <line x1="768" y1="0" x2="768" y2="16"/> \
+            <line x1="784" y1="0" x2="784" y2="16"/> \
+            <line x1="800" y1="0" x2="800" y2="16"/> \
+            <line x1="816" y1="0" x2="816" y2="16"/> \
+        </g> \
+      </svg>');
+      background-position: top center;
+      background-repeat: repeat-y;
+    }
+  }
   .max51 {
     max-width: 51rem;
-  }
-  .header {
-    line-height: 1px;
-    display: flex;
-    flex-direction: row;
-  }
-  .header::before,
-  .header::after {
-    content: "";
-    display: block;
-    flex: 1;
-    height: 1px;
-    background-color: var(--black-10);
-  }
-  .header::before {
-    flex: 1;
-  }
-  .header::after {
-    flex: 4;
   }
   </style>
 </head>
 <body class="col max80 py1 mx">
 
+  <div class="-view"></div>
+
   <header class="max51 py1 sd-px1 md-px2">
     <div class="p075">
-      <h1 class="p025 h2 lh2 fw700">lemmon juice</h1>
+      <h1 class="p025 h2 lh2 fw700"><div class="-box">lemmon juice</div></h1>
       <div class="p025">
-        <p class="lh4 color-black-50">freelance web developer</p>
+        <p class="lh4 color-black-50 -box">freelance web developer</p>
       </div>
     </div>
   </header>
@@ -46,21 +54,19 @@
 
     <section class="pt1">
 
-      <header class="py05">
-        <div class="header px1 md-py1">
-          <h1 class="fw600 mx1 color-black-50">Projects</h1>
-        </div>
-      </header>
+      <div class="md-py1">
+        <div class="px1 py05"><hr class="div color-black-10 -box"></div>
+      </div>
 
       <div class="py05"><div class="py1 md-px1">
 
-        <article class="p1">
+        <article class="p1"><div class="-box">
           <h2 class="fw600 lh4"><a class="ul" href="http://abrusco.com/">Abrusco</a></h2>
           <div class="lh4">
             <p class="color-black-80">Hackable CSS toolbox.</p>
             <p class="color-black-40">atomic css, responsive, node.js, open-source</p>
           </div>
-        </article>
+        </div></article>
         <article class="p1">
           <h2 class="fw600 lh4"><a class="ul" href="https://github.com/lemmon/babel-env-cli">Babel Env CLI</a></h2>
           <div class="lh4">
@@ -87,10 +93,10 @@
             <p class="color-black-40">email, html, saas, node.js, mongodb, serverless</p>
           </div>
         </article>
-        <article class="p1">
+        <article class="p1"><div class="-box">
           <h2 class="fw600 lh4"><a class="ul" href="https://github.com/lemmon/fetch-php">Fetch</a></h2>
           <p class="lh4 color-black-80">PHP implementation of the fetch() web api on top of Guzzle.</p>
-        </article>
+        </div></article>
 
       </div></div>
 
@@ -98,20 +104,18 @@
 
     <section class="pt1">
 
-      <header class="py05">
-        <div class="header px1 md-py1">
-          <h1 class="fw600 mx1 color-black-50">Experiments</h1>
-        </div>
-      </header>
+      <div class="md-py1">
+        <div class="px1 py05"><hr class="div color-black-10 -box"></div>
+      </div>
 
       <div class="py05"><div class="py1 md-px1">
 
         <div class="md-grid md-grid2">
-          <article class="p1">
+          <article class="p1"><div class="-box">
             <h2 class="fw600 lh4"><a class="ul" href="https://www.lemmonjuice.com/snake/">Snake</a></h2>
             <p class="lh4 color-black-80">Snake game in ASCII.</p>
             <p class="color-black-40">javascript, ascii</p>
-          </article>
+          </div></article>
           <article class="p1">
             <h2 class="fw600 lh4"><a class="ul" href="http://uuid.lemmonjuice.com/">UUID</a></h2>
             <p class="lh4 color-black-80">Version 4 UUID online generator and microservice.</p>
@@ -122,14 +126,14 @@
           <p class="lh4 color-black-80">Yet another to-do list. Ongoing experiment, work in progress.</p>
           </article>
           */ ?>
-          <article class="p1">
+          <article class="p1"><div class="-box">
             <h2 class="fw600 lh4"><a class="ul" href="http://chevron.lemmonjuice.com/">Chevron</a></h2>
             <p class="lh4 color-black-80">Online SVG chevron generator.</p>
-          </article>
-          <article class="p1">
+          </div></article>
+          <article class="p1"><div class="-box">
             <h2 class="fw600 lh4"><a class="ul" href="http://ribbon.lemmonjuice.com/">Ribbon</a></h2>
             <p class="lh4 color-black-80">Online SVG ribbon generator.</p>
-          </article>
+          </div></article>
           <?php /*
           <article class="p1">
             <div class="row items-center">
@@ -169,11 +173,9 @@
 
     <section class="pt1">
 
-      <header class="py05">
-        <div class="header px1 md-py1">
-          <h1 class="fw600 mx1 color-black-50">References</h1>
-        </div>
-      </header>
+      <div class="md-py1">
+        <div class="px1 py05"><hr class="div color-black-10 -box"></div>
+      </div>
 
       <div class="py05"><div class="py1 md-px1">
 
@@ -228,27 +230,25 @@
 
     <section class="pt1">
 
-      <header class="py05">
-        <div class="header px1 md-py1">
-          <h1 class="fw600 mx1 color-black-50">Skills</h1>
-        </div>
-      </header>
+      <div class="md-py1">
+        <div class="px1 py05"><hr class="div color-black-10 -box"></div>
+      </div>
 
       <div class="py05"><div class="py1 md-px1">
 
         <ul class="grid grid2 sd-grid3 md-grid4 lh4">
-          <li class="p1">html/css<br><span class="color-black-40"><?= date('Y') - 2003 ?> years</span></li>
-          <li class="p1">javascript<br><span class="color-black-40"><?= date('Y') - 2009 ?> years</span></li>
+          <li class="p1"><div class="-box">html/css<br><span class="color-black-40"><?= date('Y') - 2003 ?> years</span></div></li>
+          <li class="p1"><div class="-box">javascript<br><span class="color-black-40"><?= date('Y') - 2009 ?> years</span></div></li>
           <li class="p1">node.js<br><span class="color-black-40"><?= date('Y') - 2015 ?> years</span></li>
-          <li class="p1">graphql<br><span class="color-black-40"><?= date('Y') - 2016 ?> years</span></li>
-          <li class="p1">php/mysql<br><span class="color-black-40"><?= date('Y') - 2003 ?> years</span></li>
+          <li class="p1"><div class="-box">graphql<br><span class="color-black-40"><?= date('Y') - 2016 ?> years</span></div></li>
+          <li class="p1"><div class="-box">php/mysql<br><span class="color-black-40"><?= date('Y') - 2003 ?> years</span></div></li>
           <li class="p1">mongodb<br><span class="color-black-40"><?= date('Y') - 2013 ?> years</span></li>
           <li class="p1">linux</li>
           <li class="p1">git</li>
-          <li class="p1">npm</li>
+          <li class="p1"><div class="-box">npm</div></li>
           <li class="p1">open source</li>
           <li class="p1">photoshop</li>
-          <li class="p1">illustrator</li>
+          <li class="p1"><div class="-box">illustrator</div></li>
         </ul>
 
       </div></div>
@@ -278,6 +278,144 @@
       </ul>
     </nav>
   </footer>
+
+  <style media="screen">
+  .-box {
+    outline: 1px dashed var(--yellow, #ffdc00);
+  }
+  .arrow {
+    position: absolute;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    font-size: 12px;
+    line-height: 1;
+    background-color: var(--yellow, #ffdc00);
+  }
+  .arrow--horizontal {
+    height: 1px;
+    margin-top: -.5px;
+  }
+  .arrow--horizontal::before,
+  .arrow--horizontal::after {
+    content: "";
+    display: block;
+    position: absolute;
+    top: -3.5px;
+    width: 0;
+    height: 0;
+    border-top: 4px solid transparent;
+    border-bottom: 4px solid transparent;
+  }
+  .arrow--horizontal::before {
+    left: 1px;
+    border-right: 4px solid var(--yellow, #ffdc00);
+  }
+  .arrow--horizontal::after {
+    right: 1px;
+    border-left: 4px solid var(--yellow, #ffdc00);
+  }
+  .arrow--vertical {
+    writing-mode: vertical-lr;
+    text-orientation: sideways;
+    width: 1px;
+    margin-left: -.5px;
+  }
+  .arrow--vertical::before,
+  .arrow--vertical::after {
+    content: "";
+    display: block;
+    position: absolute;
+    left: -3.5px;
+    width: 0;
+    height: 0;
+    border-left: 4px solid transparent;
+    border-right: 4px solid transparent;
+  }
+  .arrow--vertical::before {
+    top: 1px;
+    border-bottom: 4px solid var(--yellow, #ffdc00);
+  }
+  .arrow--vertical::after {
+    bottom: 1px;
+    border-top: 4px solid var(--yellow, #ffdc00);
+  }
+  .arrow__caption {
+    background-color: var(--yellow, #ffdc00);
+    text-align: center;
+    position: relative;
+    z-index: 1;
+  }
+  .arrow--horizontal .arrow__caption {
+    padding: 2px 4px;
+    height: 16px;
+    margin-top: -7.5px;
+  }
+  .arrow--vertical .arrow__caption {
+    padding: 4px 2px;
+    width: 16px;
+    margin-left: -7.5px;
+  }
+  </style>
+  <script>
+  const boxes = document.getElementsByClassName('-box')
+  const n = boxes.length;
+  for (let i = 0; i < n - 1; i++) {
+    const b1 = boxes[i]
+    const b2 = boxes[i + 1]
+    arrow(b1, b2)
+  }
+  arrowEl({
+    angle: 'vertical',
+    x: boxes[0].offsetLeft + boxes[0].offsetWidth / 2,
+    y: 0,
+    h: boxes[0].offsetTop,
+  })
+  arrowEl({
+    angle: 'vertical',
+    x: boxes[n - 1].offsetLeft + boxes[n - 1].offsetWidth / 2,
+    y: boxes[n - 1].offsetTop + boxes[n - 1].offsetHeight,
+    h: document.body.offsetHeight - boxes[n - 1].offsetTop - boxes[n - 1].offsetHeight,
+  })
+  function arrow(b1, b2) {
+    // horizontal
+    const y1 = Math.max(b1.offsetTop, b2.offsetTop)
+    const y2 = Math.min(b1.offsetTop + b1.offsetHeight, b2.offsetTop + b2.offsetHeight)
+    if (y1 < y2) {
+      arrowEl({
+        angle: 'horizontal',
+        x: b1.offsetLeft + b1.offsetWidth,
+        y: y1 / 2 + y2 / 2,
+        w: b2.offsetLeft - b1.offsetLeft - b1.offsetWidth,
+      })
+    }
+    // vertical
+    const x1 = Math.max(b1.offsetLeft, b2.offsetLeft)
+    const x2 = Math.min(b1.offsetLeft + b1.offsetWidth, b2.offsetLeft + b2.offsetWidth)
+    if (x1 < x2) {
+      arrowEl({
+        angle: 'vertical',
+        x: x1 / 2 + x2 / 2,
+        y: b1.offsetTop + b1.offsetHeight,
+        h: b2.offsetTop - b1.offsetTop - b1.offsetHeight,
+      })
+    }
+  }
+  function arrowEl(props) {
+    return document.body.appendChild(Object.assign(document.createElement('div'), {
+      className: `arrow arrow--${props.angle}`,
+      style: `
+        top: ${props.y}px;
+        left: ${props.x}px;
+        width: ${props.w || 1}px;
+        height: ${props.h || 1}px;
+      `,
+    })).appendChild(Object.assign(document.createElement('div'), {
+      className: 'arrow__caption',
+      textContent: props.w || props.h,
+    }))
+  }
+  </script>
 
 </body>
 </html>
