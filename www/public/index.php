c6e8287cc34ec94f48cc98f44834c8ae98e2f784
Lemmon <lemmon@users.noreply.github.com>
2020-04-17 16:12:03 +0200

    portfolio update

--- www/public/index.php
+++ www/public/index.php
@@ -62,6 +62,18 @@
           </div>
         </article>
         <article class="p1">
+          <div class="row items-center">
+            <h2 class="fw600 lh4"><a class="ul" href="https://themes.lemmonjuice.com/">Kirby Themes</a></h2>
+            <aside class="pl025 row">
+              <div class="ml025 f4 fw600 uc bg-black-10 color-black-40 py025 px025 br2">new</div>
+            </aside>
+          </div>
+          <div class="lh4">
+            <p class="color-black-80">Collection of free and open-source themes created for Kirby CMS.</p>
+            <p class="color-black-40">html, css, responsive, kirby, cms, open-source</p>
+          </div>
+        </article>
+        <article class="p1">
           <h2 class="fw600 lh4"><a class="ul" href="https://github.com/lemmon/babel-env-cli">Babel Env CLI</a></h2>
           <div class="lh4">
             <p class="color-black-80">Command line interface for Babel + @babel/preset-env. Global installation.</p>
